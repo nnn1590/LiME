65cf1622aea90aafe18fefc0133b2e8d20f0b4a0
nnn1590 <28985763+nnn1590@users.noreply.github.com>
2020-03-22 22:04:55 +0900

    Add files

---
+++ envsetup.sh
@@ -0,0 +1,14 @@
+# Please rewrite these paths according to your environment
+# 環境に応じてパスを書き換えてください
+#
+# ドキュメント = Documents
+
+export ARCH=arm64
+export SDK_PATH=${ANDROID_HOME}
+export NDK_PATH=${NDK_ROOT}
+export KSRC_PATH=/home/${USER}/ドキュメント/msm
+export CC_PATH=/home/${USER}/android/lineageos/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+export LIME_SRC=/home/${USER}/ドキュメント/LiME/src
+export CROSS_COMPILE=/home/${USER}/android/lineageos/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/home/${USER}/android/lineageos/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export PATH="/home/${USER}/android/lineageos/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:/home/${USER}/android/lineageos/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:${PATH}"
